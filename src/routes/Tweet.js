988eca370299166f12ccd6ec3b8cd555cb484866
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-02-11 17:53:42 -0500

    feat: added tweet

---
+++ src/routes/Tweet.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { dbService } from "../firebaseService";
+import { doc, deleteDoc, setDoc, serverTimestamp } from "firebase/firestore";
+
+function Tweet({ tweetObject, userOwner }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [tweetValue, setTweetValue] = useState(tweetObject.text);
+
+  const deleteTweet = async () => {
+    const ok = window.confirm("Are you sure you want to delete?");
+    if (ok) {
+      await deleteDoc(doc(dbService, "CRUD", tweetObject.id));
+    }
+  };
+
+  const toggleEdit = () => {
+    setIsEditing((prev) => !prev);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await setDoc(doc(dbService, "CRUD", tweetObject.id), {
+      createID: userOwner.uid,
+      createdAt: serverTimestamp(),
+      text: tweetValue,
+    });
+
+    setIsEditing(false);
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+
+    setTweetValue(value);
+  };
+
+  return (
+    <div>
+      {isEditing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              value={tweetValue}
+              onChange={onChange}
+              placeholder="Edit..."
+            />
+            <input type="submit" value="Update" />
+          </form>
+          <button onClick={toggleEdit}> Cancel </button>
+        </>
+      ) : (
+        <>
+          <h4>{tweetObject.text}</h4>
+          {tweetObject.attachmentUrl && (
+            <img
+              src={tweetObject.attachmentUrl}
+              width="50px"
+              height="50px"
+              alt=""
+            />
+          )}
+          {userOwner.uid === tweetObject.createID && (
+            <div>
+              <button onClick={deleteTweet}>Delete</button>
+              <button onClick={toggleEdit}>Edit</button>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Tweet;
