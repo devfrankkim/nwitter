ff3dcb16af8caf5d8bc155d5a841bee27ef469f5
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-02-13 13:51:10 -0500

    fixing bugs

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-function Navigation() {
+function Navigation({ userOwner }) {
   return (
     <nav>
-      <ul>
+      <ul style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" style={{ marginRight: 10 }}>
+            <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />
+          </Link>
         </li>
         <li>
-          <Link to="/profile">Profile</Link>
+          {userOwner &&
+            (userOwner.displayName ? (
+              <Link to="/profile">{userOwner.displayName}'s profile</Link>
+            ) : (
+              <Link to="/profile"> {userOwner.email} </Link>
+            ))}
         </li>
       </ul>
     </nav>
